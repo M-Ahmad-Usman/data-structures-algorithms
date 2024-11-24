b5be7344e2ae409539a73bb3d6ca8911552f6cf7
M-Ahmad-Usman <developer.ahmad@proton.me>
2024-11-24 21:26:55 +0500

    complete QNo10

---
+++ Lab02/QNo10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void Sum (int *ptr1, int *ptr2)
+{
+    *ptr1 += *ptr2;
+    *ptr2 = *ptr1;
+}
+
+int main ()
+{
+    int a, b;
+    cout << "Enter the numbers: \n";
+    cin >> a >> b;
+
+    Sum (&a, &b);
+    cout << a << "\n" << b;
+
+    char screenshot;
+	cin >> screenshot; // For taking output's screenshot
+    return 0;
+}
